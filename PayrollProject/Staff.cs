5d888bfccc98100d4043ba36c9d553ba0b41e58c
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-08 17:43:24 -0700

    Created staff class

---
+++ PayrollProject/Staff.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace PayrollProject
+{
+    public class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        public float TotalPay { get; protected set; }
+        public float BasePay { get; private set; }
+        public string NameOfStaff { get; private set; }
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (hWorked > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        public Staff(string name, float rate)
+        {
+            name = NameOfStaff;
+            rate = hourlyRate;
+        }
+
+        public virtual void CalculatePay()
+        {
+            WriteLine("Calculating Pay...");
+            BasePay = hWorked * hourlyRate;
+            TotalPay = BasePay;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {NameOfStaff} \n Hours Worked: {HoursWorked} \n Base Pay: {BasePay} \n Total Pay: {TotalPay}";
+        }
+    }
+}
