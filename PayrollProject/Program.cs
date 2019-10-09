cf46bbc81bbb6b740e21f7da0900c480fe76a1ef
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-08 17:20:01 -0700

    initial commit

---
+++ PayrollProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
