1f41ce5b7ddafeb314fe0ecdf10a66f179f565fa
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-09 11:39:01 -0700

    Completed Staff subclasses of Manager and Admin

---
+++ PayrollProject/Admin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PayrollProject
+{
+    public class Admin : Staff
+    {
+        private const float overtimeRate = 15.5F;
+        private const float adminHourlyRate = 30F;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base(name, adminHourlyRate)
+        {
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if (HoursWorked > 160)
+            {
+                Overtime = (HoursWorked - 160) * overtimeRate;
+                TotalPay = BasePay + Overtime;
+            }
+            else
+            {
+                Overtime = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Admin Payroll Mode \n Name: {NameOfStaff} \n Hours Worked: {HoursWorked} \n Base Pay: {BasePay} \n Overtime Hours: {Overtime} \n Total Pay: {TotalPay}";
+        }
+    }
+}
---
+++ PayrollProject/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PayrollProject
+{
+    public class Manager : Staff
+    {
+        private const float managerHourlyRate = 50;
+        public int Allowance { get; private set; }
+
+        public Manager(string name) : base(name, managerHourlyRate)
+        {
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Allowance = 1000;
+
+            if (HoursWorked > 160)
+                TotalPay = BasePay + Allowance;
+        }
+
+        public override string ToString()
+        {
+            return $"Manager Payroll Mode \n Name: {NameOfStaff} \n Hours Worked: {HoursWorked} \n Base Pay: {BasePay} \n Allowance: {Allowance} \n Total Pay: {TotalPay}";
+        }
+    }
+}
