ee3f1afb43ac2959908ff010bed4b9fd96f09d71
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-09 18:35:51 -0700

    Completed Program.Main and PaySlip class

---
+++ PayrollProject/PaySlip.cs
@@ -0,0 +1,79 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PayrollProject
+{
+    public class PaySlip
+    {
+        private int month;
+        private int year;
+
+        enum MonthsOfYear { JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC }
+
+        public PaySlip(int payMonth, int payYear)
+        {
+            month = payMonth;
+            year = payYear;
+        }
+
+        public void GeneratePaySlip(List<Staff> myStaff)
+        {
+            string path;
+
+            foreach (Staff n in myStaff)
+            {
+                path = $"{n.NameOfStaff}.txt";
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    WriteLine($"PAYSLIP FOR {(MonthsOfYear)month} {year}");
+                    WriteLine("==============================");
+                    WriteLine($"Name of Staff: {n.NameOfStaff}");
+                    WriteLine($"Hours Worked: {n.HoursWorked}\n");
+                    WriteLine($"Base Pay: {n.BasePay}");
+
+                    if (n.GetType() == typeof(Manager))
+                    {
+                        WriteLine($"Allowance: {((Manager)n).Allowance}\n");
+                    }
+                    else if (n.GetType() == typeof(Admin))
+                    {
+                        WriteLine($"Hours of Overtime: {((Admin)n).Overtime}\n");
+                    }
+
+                    WriteLine("==============================");
+                    WriteLine($"Total Pay: {n.TotalPay:C}");
+                    WriteLine("==============================");
+
+                    writer.Close();
+                }
+            }
+        }
+
+        public void GenerateSummary(List<Staff> myStaff)
+        {
+            var result = from n in myStaff
+                         where n.HoursWorked < 10
+                         orderby n.NameOfStaff ascending
+                         select new { n.NameOfStaff, n.HoursWorked };
+
+            string path = "summary.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                WriteLine("Staff w/ less than 10 working hours this month\n");
+
+                foreach (var n in result)
+                {
+                    WriteLine($"Name: {n.NameOfStaff}, Hours Worked: {n.HoursWorked}");
+                }
+                writer.Close();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Month: {month}, Year: {year}";
+        }
+    }
+}
--- PayrollProject/Program.cs
+++ PayrollProject/Program.cs
@@ -1,12 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace PayrollProject
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Staff> myStaff = new List<Staff>();
+            FileReader fr = new FileReader();
+            int month = 0;
+            int year = 0;
+
+            while (year == 0)
+            {
+                Write("\nPlease enter the year: ");
+
+                try
+                {
+                    string userInput = ReadLine();
+                    year = Convert.ToInt32(userInput);
+                }
+                catch (FormatException e)
+                {
+                    WriteLine(e.Message);
+                }
+            }
+
+            while (month == 0)
+            {
+                Write("\nPlease enter the month: ");
+
+                try
+                {
+                    string userInput = ReadLine();
+                    month = Convert.ToInt32(userInput);
+
+                    if (month < 1 || month > 12)
+                    {
+                        WriteLine("Invalid month input");
+                        month = 0;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    WriteLine(e.Message);
+                }
+            }
+
+            myStaff = fr.ReadFile();
+
+            for (int i = 0; i < myStaff.Count; i++)
+            {
+                var employee = myStaff[i];
+
+                try
+                {
+                    WriteLine($"Enter hours worked for {employee.NameOfStaff}");
+                    string userInput = ReadLine();
+                    employee.HoursWorked = Convert.ToInt32(userInput);
+                    employee.CalculatePay();
+                    WriteLine(employee);
+                }
+                catch (Exception e)
+                {
+                    WriteLine(e.Message);
+                    i--;
+                }
+            }
+
+            PaySlip ps = new PaySlip(month, year);
+            ps.GeneratePaySlip(myStaff);
+            ps.GenerateSummary(myStaff);
+
+            Read();
         }
     }
 }
